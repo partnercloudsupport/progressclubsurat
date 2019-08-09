3b8ee26584c12e74a32bf9dd9f10ab721b25211c
danish <singhkapil708@gmail.com>
2019-08-09 15:43:55 +0530

    event details by chirag

--- lib/Screens/EventDetail.dart
+++ lib/Screens/EventDetail.dart
@@ -37,77 +37,135 @@ class _EventDetailState extends State<EventDetail> {
               fontWeight: FontWeight.w600),
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.only(left: 8, right: 8),
-        child: Card(
-          shape: RoundedRectangleBorder(
-            //side: BorderSide(color: cnst.appcolor)),
-            side: BorderSide(width: 0.50, color: cnst.appPrimaryMaterialColor),
-            borderRadius: BorderRadius.circular(
-              10.0,
-            ),
-          ),
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  '${widget.Event["Title"]}',
-                  style: TextStyle(
-                      fontSize: 16,
-                      color: cnst.appPrimaryMaterialColor,
-                      fontWeight: FontWeight.w600),
-                ),
-                Row(
-                  children: <Widget>[
-                    Image.asset(
-                      'images/icon_Event.png',
-                      height: 18,
-                      width: 18,
-                    ),
-                    Expanded(
-                        child: Padding(
-                      padding: const EdgeInsets.only(left: 5),
-                      child: Text(
-                        '${widget.Event["EventDate"].substring(8, 10)}-'
-                        "${new DateFormat.MMM().format(DateTime.parse(DateFormat("yyyy-MM-dd").parse(widget.Event["EventDate"].substring(0, 10)).toString()))}-${widget.Event["EventDate"].substring(0, 4)}",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: cnst.appPrimaryMaterialColor,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(left: 5, right: 5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: widget.Event["Photo"] != null
+                        ? FadeInImage.assetNetwork(
+                            placeholder: 'images/icon_events.jpg',
+                            image: widget.Event["Photo"],
+                            fit: BoxFit.fill,
+                            height: 180,
+                            width: MediaQuery.of(context).size.width,
+                          )
+                        : Image.asset(
+                            'images/icon_events.jpg',
+                            fit: BoxFit.fill,
+                            height: 180,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                  ),
+                  Container(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        //side: BorderSide(color: cnst.appcolor)),
+                        side: BorderSide(
+                            width: 0.20, color: cnst.appPrimaryMaterialColor),
+                        borderRadius: BorderRadius.circular(
+                          6.0,
                         ),
                       ),
-                    ))
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: widget.Event["Photo"] != null
-                      ? FadeInImage.assetNetwork(
-                          placeholder: 'images/icon_Events.jpg',
-                          image: widget.Event["Photo"],
-                          fit: BoxFit.fill,
-                          height: 180,
-                          width: MediaQuery.of(context).size.width - 20,
-                        )
-                      : Image.asset(
-                          'images/icon_Events.jpg',
-                          fit: BoxFit.fill,
-                          height: 180,
-                          width: MediaQuery.of(context).size.width - 20,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                '${widget.Event["Title"]}',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: cnst.appPrimaryMaterialColor,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Image.asset(
+                                    'images/icon_event.png',
+                                    height: 18,
+                                    width: 18,
+                                  ),
+                                  Expanded(
+                                      child: Padding(
+                                    padding: const EdgeInsets.only(left: 5),
+                                    child: Text(
+                                      '${widget.Event["EventDate"].substring(8, 10)}-'
+                                      "${new DateFormat.MMM().format(DateTime.parse(DateFormat("yyyy-MM-dd").parse(widget.Event["EventDate"].substring(0, 10)).toString()))}-${widget.Event["EventDate"].substring(0, 4)}",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: cnst.appPrimaryMaterialColor,
+                                      ),
+                                    ),
+                                  ))
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10, bottom: 5),
-                  child: Text(
-                    '${widget.Event["LongDesc"]}',
-                    maxLines: 3,
-                    textAlign: TextAlign.left,
-                    style: TextStyle(fontSize: 13),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    //side: BorderSide(color: cnst.appcolor)),
+                    side: BorderSide(
+                        width: 0.20, color: cnst.appPrimaryMaterialColor),
+                    borderRadius: BorderRadius.circular(
+                      6.0,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: Text(
+                            '${widget.Event["ShortDesc"]}',
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(fontSize: 13),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, bottom: 5),
+                          child: Text(
+                            'Description',
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: cnst.appPrimaryMaterialColor,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(top: 5, bottom: 5, left: 8),
+                          child: Text(
+                            '${widget.Event["LongDesc"]}',
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(fontSize: 13),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
